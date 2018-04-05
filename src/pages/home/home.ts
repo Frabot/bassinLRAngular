ad4fe17cd85696d7ca543fe9027a7b875b851697
frabot <rabot.fanny@gmail.com>
2018-04-05 16:07:49 +0200

    Completing landing page + Creating MuseePage, MeteoPage, TemoignagePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BateauPage} from "../bateau/bateau";
+import {MuseePage} from "../musee/musee";
+import {MeteoPage} from "../meteo/meteo";
+import {TemoignagePage} from "../temoignage/temoignage";
 
 @Component({
   selector: 'page-home',
@@ -14,4 +18,22 @@ export class HomePage {
   ngOnInit() {
 
   }
+
+    goToPage(page){
+      console.log("AHHHHH");
+        switch(page){
+            case "Musee":
+                this.navCtrl.push(MuseePage);
+                break;
+            case "Bateaux":
+                this.navCtrl.push(BateauPage);
+                break;
+            case "Meteo":
+                this.navCtrl.push(MeteoPage);
+                break;
+            case "Temoignage":
+                this.navCtrl.push(TemoignagePage);
+                break;
+        }
+    }
 }
